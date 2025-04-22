259b9f00494317dd80fedaa3c062b0c0a0a04138
renan1811 <rennlima477@gmail.com>
2025-04-22 16:58:39 -0300

    nova section livros

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,62 @@
 <script setup>
-
+const produtos = [
+    {
+        id: 1,
+        titulo: 'Chain of Iron: Volume 2',
+        resenha: 'Cassandra Clare',
+        preco: 23.24,
+        capa: 'https://cdn.kobo.com/book-images/6db37b19-2d7d-4e5b-a1d8-b006188c9db4/1200/1200/False/the-last-hours-chain-of-iron.jpg',
+    },
+    {
+        id: 2,
+        titulo: 'Chain of Thorns',
+        resenha: 'Cassandra Clare',
+        preco: 23.24,
+        capa: 'https://cdn.kobo.com/book-images/4aa958d8-c1ed-4bf2-90ce-fef019f92a15/353/569/90/False/the-last-hours-chain-of-thorns.jpg',
+    },
+    {
+        id: 3,
+        titulo: 'City of Fallen Angels',
+        resenha: 'Cassandra Clare',
+        preco: 13.94,
+        capa: 'https://m.media-amazon.com/images/I/815MzJpG6iL._AC_UF1000,1000_QL80_.jpg',
+    },
+    {
+        id: 4,
+        titulo: 'Nona the Ninth',
+        resenha: 'Cassandra Clare',
+        preco: 16.84,
+        capa: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThSjYxA73VNaIFSItXwUsuMHkRQo7f8PXGBg&s',
+    },
+    {
+        id: 5,
+        titulo: 'Harlem Shuffle',
+        resenha: 'Colson Whitehead',
+        preco: 26.92,
+        capa: 'https://m.media-amazon.com/images/I/81ZPFCh0xML.jpg',
+    },
+    {
+        id: 6,
+        titulo: 'Two Old Women',
+        resenha: 'Velma Wallis',
+        preco: 13.95,
+        capa: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBXEMa4hM454G7Whh7PrDN8R_oYebpPdFl_Q&s',
+    },
+    {
+        id: 7,
+        titulo: 'Carrie Soto Is Back',
+        resenha: 'Taylor Jenkins Reid',
+        preco: 26.04,
+        capa: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGJROWBwFV4AHVxK1H0NNVTiEBBlVbmnf2gg&s',
+    },
+    {
+        id: 8,
+        titulo: 'Book Lovers',
+        resenha: 'Emily Henry',
+        preco: 15.81,
+        capa: 'https://m.media-amazon.com/images/I/71Xy4AL7jKL.jpg',
+    }
+];
 </script>
 
 <template>
@@ -47,10 +104,24 @@
       </ul>
     </div>
   </header>
+  <body>
+    <section class="listalivros">
+    <h1>Lista de Livros</h1>
+    <div v-for="produto in produtos" :key="produto.id" class="livro">
+      <img :src="produto.capa" alt="" width="100%" height="100%" />
+      <h2>{{ produto.titulo }}</h2>
+      <p class="res">{{ produto.resenha }}</p>
+        <strong>R$ {{ produto.preco.toFixed(2) }}</strong>
+        <br>
+      <button class="compra">
+            <i class="fa-solid fa-cart-shopping"></i>
+            <p>comprar</p>
+      </button>      
+    </div>
+  </section>
 
-  <main>
-
-  </main>
+  </body>
+  
 </template>
 
 <style scoped>
@@ -58,20 +129,28 @@
 li {
   list-style-type: none;
 }
+* {
+  font-family: 'SuaFonte', sans-serif; /* Substitua 'SuaFonte' pela fonte que você quer */
+}
 
-
+/* Garantindo que os ícones Font Awesome usem sua própria fonte */
+i, .fa {
+  font-family: 'Font Awesome 5 Free', 'Font Awesome 5 Free Solid', 'FontAwesome', sans-serif !important;
+}
 
 /*                     HEADER                   */
 header {
   display: flex;
   justify-content: center;
-
+  border-bottom: 2px solid #27AE60;
+  
 }
 
 /*tituluzinho*/
 div.titulo {
   display: flex;
-  margin: 1vw 3vw 0 0;
+  margin: 1vw 3vw 4px 0;
+
 }
 
 div.titulo p.um {
@@ -96,10 +175,10 @@ div.titulo p.dois {
       width: 100%;
       padding: 10px 10px 10px 35px; /* espaço para o ícone */
       font-size: 16px;
-      border: 1px solid gre;
+      border: none;
       border-radius: 4px;
       margin: 1.5vw 0 0 0;
-      background-color: #747474;
+      background-color: #F1F1F1;
     }
 
     .icone-pesquisa {
@@ -121,12 +200,14 @@ div ul.page {
 
 div ul.page li {
   margin: 1vw 2vw 0 3vw;
+  color: #7B7881;
 }
 
 /*iconezinhos*/
 div ul.icon {
   display: flex;
-  justify-content: space-between
+  justify-content: space-between;
+  color: #27AE60;
 }
 
 div ul.icon li {
@@ -138,4 +219,53 @@ div ul.icon li.icone {
   border-left: 1px solid;
   padding: 0 1vw 0 1vw;
 }
+/* SECTION LIVROSSSSSSSSSSSSSSSSSSSSSSSS*/
+section.listalivros {
+  margin: 5vw;
+}
+
+section.listalivros h1 {
+  font-size: 2rem;
+  color: #231F2D;
+}
+
+
+section.listalivros .livro {
+  display: inline-block;
+  width: 20%;
+  margin: 1.5vw 1.5vw; 
+}
+
+section.listalivros .livro img {
+  border-radius: 5px;
+}
+
+section.listalivros .livro h2 {
+  font-size: 1.7rem;
+  color: #231F2D;
+}
+
+section.listalivros .livro p.res {
+  color: #7B7881;
+}
+
+section.listalivros .livro strong {
+  
+}
+section.listalivros .livro .compra {
+  background-color: #27AE60;
+  width: 100%;
+  height: 40px;
+  color: white;
+  padding: 10px;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  font-size: 1.1rem;
+  margin-top: 1vw;
+}
+
 </style>
