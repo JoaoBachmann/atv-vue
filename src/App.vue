d429124fb2ebc167430b42a8404968fa008fc185
JoaoBachmann <joaobachmannaluno@gmail.com>
2025-04-22 17:01:20 -0300

    finalização do footer e section opcoes

--- src/App.vue
+++ src/App.vue
@@ -38,11 +38,40 @@
         <p>Mais vendidos</p>
       </div>
     </section>
-
-    <section class="livros">
-      <h1>Lançamentos</h1>
-    </section>
   </main>
+  <footer>
+    <div class="pe">
+      <div class="unha">
+        <p>IFbooks</p>
+        <span class="fa-brands fa-square-facebook"></span>
+        <span class="fa-brands fa-square-instagram"></span>
+        <span class="fa-brands fa-square-x-twitter"></span>
+      </div>
+      <div class="ifbook">
+        <p>Contato</p>
+        <div class="redes">
+          <span class="fa-solid fa-phone"></span>
+          <p>+55 47 40045263</p>
+        </div>
+        <div class="redes">
+          <span class="fa-solid fa-clock"></span>
+          <p>8h às 23h - Seg a Sex</p>
+        </div>
+        <div class="redes">
+          <span class="fa-solid fa-envelope"></span>
+          <p>contato@ifbooks.com</p>
+        </div>
+        <div class="img">
+          <img src="../public/Imagens/paipal 1.png" alt="paypal">
+          <img src="../public/Imagens/MasterCard-Logo-1979 1.png" alt="Master Card">
+          <img src="../public/Imagens/VISA-card-logo- 1.png" alt="Visa">
+        </div>
+      </div>
+    </div>
+    <div class="theLast">
+      <P>© Alguns direitos reservados. IFbooks 2025. </P>
+    </div>
+  </footer>
 </template>
 <style scoped>
 /*SECTION AUTOR*/
@@ -92,24 +121,91 @@
 }
 
 /*Section OPCOES*/
-.opcoes{
-  padding: 3vw 0 ;
+.opcoes {
+  padding: 3vw 0;
   display: flex;
   justify-content: space-between;
   border-top: 2px solid #27ae60;
   border-bottom: 2px solid #27ae60;
 }
 
-.opcoes div{
-  padding: 0 10vw 0 ;
+.opcoes div {
+  padding: 0 10vw 0;
   display: flex;
-  align-items: center; /* Alinha verticalmente */
-  gap: 1vw; /* Espaço entre o ícone e o texto */
+  align-items: center;
+  /* Alinha verticalmente */
+  gap: 1vw;
+  /* Espaço entre o ícone e o texto */
   font-size: 1.2vw;
   font-weight: bolder;
 }
 
-.linha{
+.linha {
   border-right: 1px solid grey;
+}
+
+/*FOOTER*/
+footer {
+  background-color: #27ae60;
+}
+
+.pe {
+  display: flex;
+  justify-content: space-between;
+}
+
+.pe {
+  padding: 1.5vw 5vw;
+  border-bottom: 1px solid white;
+}
+
+/*lado esquerdo*/
+.pe .unha {
+  color: white;
+  font-size: 1.2vw;
+}
+
+.pe .unha span {
+  padding: 0 7px 0 0;
+}
+
+/*lado direito*/
+.ifbook p {
+  color: white;
+  font-size: 1.1vw;
+}
+
+.ifbook span {
+  color: white;
+  font-size: 1.1vw;
+}
+
+.ifbook .redes {
+  display: flex;
+  align-items: center;
+  /* Alinha verticalmente */
+}
+
+.redes span {
+  padding: 0 10px 0 0;
+}
+
+/*cartoes*/
+.img {
+  padding: 2vw 0;
+}
+
+.img img {
+  padding: 0 0.5vw;
+}
+
+/*theLast*/
+
+.theLast{
+  color: #FFFFFF;
+  font-size: 1.2vw;
+  display: flex;
+  justify-content: center;
+  padding: 2vw;
 }
 </style>
